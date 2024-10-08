2ff5a3c3ecff3eaff6d9e6a77273078fa91d9445
plamenna-petrova <plamennavp@abv.bg>
2024-10-08 17:13:06 +0300

    Typed version of inter font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,21 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { NextFont } from "next/dist/compiled/@next/font";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const interFont: NextFont = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
     title: "WordPress As A Headless CMS Demo",
     description: "Using WordPress As A Headless CMS",
 };
 
-export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={interFont.className}>{children}</body>
         </html>
     );
 }
+
+export default RootLayout;
