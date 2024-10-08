d759997de7adb6acd27e3bfd4896f9dd482a0499
plamenna.petrova <plamennavp@abv.bg>
2024-10-08 21:56:00 +0300

    Added query-string library and created getUrl builder function and as well as a function for retrieving all posts based on filter values

--- lib/wordpressRequests.ts
+++ lib/wordpressRequests.ts
@@ -1,6 +1,28 @@
+import { Post } from "@/types/Post";
+import queryString from "query-string";
 
-const baseUrl = 'localhost/wordpress';
-
+const baseUrl = 'http://wordpress.local/';
+ 
 const getUrl = (path: string, query?: Record<string, any>): string => {
-    return ``;
+    const queryParameters: string | null = query ? queryString.stringify(query) : null;
+    return `${baseUrl}${path}${queryParameters ? `?${queryParameters}` : ''}`;
+}
+
+export const getAllPosts = async (filterParameters: { 
+    author?: string;
+    tag?: string;
+    category?: string
+}): Promise<Post[]> => {
+    const postsUrl = getUrl('/wp-json/wp/v2/posts', 
+        { 
+            author: filterParameters?.author, 
+            tags: filterParameters.tag, 
+            categories: filterParameters.category 
+        }
+    );
+    
+    const postsResponse = await fetch(postsUrl);
+    const posts: Post[] = await postsResponse.json();
+    
+    return posts;
 }
