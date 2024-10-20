d43cf999dc74705bde0eb9c4d53d9a35e61d3ee4
plamenna.petrova <plamennavp@abv.bg>
2024-10-20 16:52:52 +0300

    Added a post card component and allowed fetching of featured media through next.config.mjs

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,8 +1,10 @@
 import Container from "@/components/container/container";
+import PostCard from "@/components/posts/post-card";
 import Section from "@/components/section/section";
+import { Pagination, PaginationContent, PaginationItem, PaginationLink, PaginationNext, PaginationPrevious } from "@/components/ui/pagination";
 import { getAllAuthors, getAllCategories, getAllPosts, getAllTags } from "@/lib/wordpressRequests";
 
-const Posts = async ({ searchParams }: { searchParams: { [key: string]: string | undefined }}) => {
+const Posts = async ({ searchParams }: { searchParams: { [key: string]: string | undefined } }) => {
     const { author, tag, category, page } = searchParams;
     const posts = await getAllPosts({ author, tag, category });
     const authors = await getAllAuthors();
@@ -19,13 +21,48 @@ const Posts = async ({ searchParams }: { searchParams: { [key: string]: string |
         <Section>
             <Container>
                 <h1>Posts</h1>
+
                 {paginatedPosts.length > 0 ? (
                     <div className="grid md:grid-cols-3 gap-4 z-0">
-                        {JSON.stringify(paginatedPosts, null, 2)}
+                        {paginatedPosts.map((post: any) => (
+                            <PostCard key={post.id} post={post} />
+                        ))}
                     </div>
                 ) : (
-                    <div>No Results</div>
+                    <div className="h-24 w-full border rounded-lg bg-accent/25 flex items-center justify-center">
+                        <p>No Results Found</p>
+                    </div>
                 )}
+
+                <div className="mt-8 not-prose">
+                    <Pagination>
+                        <PaginationContent>
+                            <PaginationItem>
+                                <PaginationPrevious
+                                    className={currentPage === 1 ? "pointer-events-none text-muted" : ""}
+                                    href={`/posts?page=${Math.max(currentPage - 1, 1)}${category ? `&category=${category}` : ""
+                                        }${author ? `&author=${author}` : ""}${tag ? `&tag=${tag}` : ""
+                                        }`}
+                                />
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationLink href={`/posts?page=${currentPage}`}>
+                                    {page}
+                                </PaginationLink>
+                            </PaginationItem>
+                            <PaginationItem>
+                                <PaginationNext
+                                    className={
+                                        currentPage === totalPages ? "pointer-events-none text-muted" : ""
+                                    }
+                                    href={`/posts?page=${Math.min(currentPage + 1, totalPages)}${category ? `&category=${category}` : ""
+                                        }${author ? `&author=${author}` : ""}${tag ? `&tag=${tag}` : ""
+                                        }`}
+                                />
+                            </PaginationItem>
+                        </PaginationContent>
+                    </Pagination>
+                </div>
             </Container>
         </Section>
     )
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "http",
+                hostname: 'wordpress.local',
+                port: "",
+                pathname: "/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
