e61b66117a6a41a527fad63084b117325a78e586
plamenna-petrova <plamennavp@abv.bg>
2024-12-20 16:12:49 +0200

    Removed comments

--- app/documentation/layout.tsx
+++ app/documentation/layout.tsx
@@ -3,20 +3,15 @@ import { type Section } from '@/components/documentation/SectionProvider';
 import { DocumentationLayout } from '@/components/documentation/DocumentationLayout';
 
 export default async function Documentation({ children }: { children: React.ReactNode }) {
-  // Glob all MDX files from root
   let pages = await glob('documentation/**/*.mdx');
 
-  console.log('Pages:', pages); // Debugging: Verify paths
-
-  // Use dynamic import based on root-relative paths
   let documentationSectionsEntries = (await Promise.all(
     pages.map(async (filename) => [
       '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
-      (await import(`./${filename}`)).sections, // Import based on app as root
+      (await import(`./${filename}`)).sections,
     ]),
   )) as Array<[string, Array<Section>]>;
-
-  // Convert entries to an object
+  
   let documentationSections = Object.fromEntries(documentationSectionsEntries);
 
   return (
