fe0058ec961683cc65f35a412de352a2dea1d653
plamenna-petrova <plamennavp@abv.bg>
2024-12-20 16:12:02 +0200

    Fixed the loading of the documenation page.mdx with glob

--- app/documentation/layout.tsx
+++ app/documentation/layout.tsx
@@ -1,23 +1,29 @@
-import glob from 'fast-glob'
-
+import glob from 'fast-glob';
 import { type Section } from '@/components/documentation/SectionProvider';
 import { DocumentationLayout } from '@/components/documentation/DocumentationLayout';
 
 export default async function Documentation({ children }: { children: React.ReactNode }) {
-  let pages = await glob('**/*.mdx', { cwd: 'app' });
+  // Glob all MDX files from root
+  let pages = await glob('documentation/**/*.mdx');
 
+  console.log('Pages:', pages); // Debugging: Verify paths
+
+  // Use dynamic import based on root-relative paths
   let documentationSectionsEntries = (await Promise.all(
     pages.map(async (filename) => [
       '/' + filename.replace(/(^|\/)page\.mdx$/, ''),
-      (await import(`./${filename}`)).sections,
+      (await import(`./${filename}`)).sections, // Import based on app as root
     ]),
   )) as Array<[string, Array<Section>]>;
-  
+
+  // Convert entries to an object
   let documentationSections = Object.fromEntries(documentationSectionsEntries);
 
   return (
     <div className="w-full mx-auto">
-      <DocumentationLayout allSections={documentationSections}>{children}</DocumentationLayout>
+      <DocumentationLayout allSections={documentationSections}>
+        {children}
+      </DocumentationLayout>
     </div>
   );
 }
