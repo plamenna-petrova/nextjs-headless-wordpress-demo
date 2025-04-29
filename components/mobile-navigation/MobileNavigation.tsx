29209eed69ee9d108950529616deb06517a8ab17
plamenna-petrova <plamennavp@abv.bg>
2025-04-29 15:11:44 +0300

    Changed mobile navigation links

--- components/mobile-navigation/MobileNavigation.tsx
+++ components/mobile-navigation/MobileNavigation.tsx
@@ -61,14 +61,14 @@ const MobileNavigation = () => {
         </MobileLink>
         <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
           <div className="flex flex-col space-y-3">
-            <h3 className="text-small mt-6">Основно Меню</h3>
+            <h3 className="text-small mt-6">Приложение</h3>
             <Separator />
             {Object.entries(mainMenu).map(([key, href]) => (
               <MobileLink key={key} href={href} onOpenChange={setIsMobileNavigationOpen}>
                 {key.charAt(0).toUpperCase() + key.slice(1)}
               </MobileLink>
             ))}
-            <h3 className="text-small pt-6">Блог</h3>
+            <h3 className="text-small pt-6">Полезни линкове</h3>
             <Separator />
             {Object.entries(contentMenu).map(([key, href]) => (
               <MobileLink key={key} href={href} onOpenChange={setIsMobileNavigationOpen}>
